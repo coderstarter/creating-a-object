8d7f2aac942d0ed14358f00317390d06f9bd3526
Abhishek Sharma <coder8248@gmail.com>
2022-10-22 22:28:08 +0530

    Adding the usage of function and how to change the value

--- app.js
+++ app.js
@@ -8,15 +8,14 @@ const laptop = {
         pixel_density: 720
     },
     screenwork: false,
-    usage: function (uses, functional) {
-        this.screenwork = uses;
-        this.functional = functional;
-    }
+    usage: function (useage, functionn) {
+        this.resolutions.pixels = useage;
+        this.resolutions.pixel_density = functionn;
+    },
 };
 
 console.log(laptop)
 console.log(laptop.name)
-laptop.usage('Used to monitor', 'Computational work')
+laptop.usage(1000, 99);
 console.log(laptop)
-console.log(laptop.usage)
-console.log(laptop.usage.uses)
+console.log(laptop.resolutions.pixels)
